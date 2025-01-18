ceaf3177d06336de9663f372c518c3b63e5c1c70
hanna <hannadhm@hotmail.co.uk>
2025-01-18 15:18:58 +0000

    Revert "modify vite.config.ts to make react router intercept 404"
    This reverts commit 4508776ffdfe8f1bf2d1977857cf5c9cfa61fd23.

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/hanna-portfolio/",
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          "404": ["@vitejs/plugin-vue"],
-        },
-      },
-    },
-  },
   plugins: [react()],
 });
