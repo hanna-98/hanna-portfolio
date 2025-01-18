dd2812b826f3b2e53abf20546e2e6c7fbdc08e6c
hanna <hannadhm@hotmail.co.uk>
2025-01-18 17:25:18 +0000

    replace href with to

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,24 +4,24 @@ export default function Navigation() {
   return (
     <Navbar className="sticky top-0 z-10 flex h-20 justify-center border-blue-950 bg-globalBg pb-16 pt-6">
       <Navbar.Collapse className="flex justify-center">
-        <Navbar.Link className="navbar-link text-textColour text-6xl" href="/">
+        <Navbar.Link className="navbar-link text-6xl text-textColour" to="/">
           HOME
         </Navbar.Link>
         <Navbar.Link
-          className="navbar-link text-textColour text-6xl"
-          href="/about-me"
+          className="navbar-link text-6xl text-textColour"
+          to="/about-me"
         >
           ABOUT ME
         </Navbar.Link>
         <Navbar.Link
-          className="navbar-link text-textColour text-6xl"
-          href="/projects"
+          className="navbar-link text-6xl text-textColour"
+          to="/projects"
         >
           PROJECTS
         </Navbar.Link>
         <Navbar.Link
-          className="navbar-link text-textColour text-6xl"
-          href="/experience"
+          className="navbar-link text-6xl text-textColour"
+          to="/experience"
         >
           EXPERIENCE
         </Navbar.Link>
