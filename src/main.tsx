525441897f9d500a859a4acb6d8a2809200fc7c8
hanna <hannadhm@hotmail.co.uk>
2025-01-18 17:22:50 +0000

    Revert "remove basename from HashRouter"
    This reverts commit 5c4ba07ec78b76f19bd4bcfe9b7b615c86b1e122.

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HashRouter>
+    <HashRouter basename="/hanna-portfolio">
       <App />
     </HashRouter>
   </React.StrictMode>,
