61392266f62f6e57337f65b1b456ff76188c4fcd
hanna <hannadhm@hotmail.co.uk>
2025-01-18 17:35:33 +0000

    wrap links in Link from react-router-dom

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,29 +1,21 @@
 import { Navbar } from "flowbite-react";
+import { Link } from "react-router-dom";
 
 export default function Navigation() {
   return (
     <Navbar className="sticky top-0 z-10 flex h-20 justify-center border-blue-950 bg-globalBg pb-16 pt-6">
       <Navbar.Collapse className="flex justify-center">
-        <Navbar.Link className="navbar-link text-6xl text-textColour" to="/">
-          HOME
+        <Navbar.Link className="navbar-link text-6xl text-textColour">
+          <Link to="/">HOME</Link>
         </Navbar.Link>
-        <Navbar.Link
-          className="navbar-link text-6xl text-textColour"
-          to="/about-me"
-        >
-          ABOUT ME
+        <Navbar.Link className="navbar-link text-6xl text-textColour">
+          <Link to="/about-me">ABOUT ME</Link>
         </Navbar.Link>
-        <Navbar.Link
-          className="navbar-link text-6xl text-textColour"
-          to="/projects"
-        >
-          PROJECTS
+        <Navbar.Link className="navbar-link text-6xl text-textColour">
+          <Link to="/projects">PROJECTS</Link>
         </Navbar.Link>
-        <Navbar.Link
-          className="navbar-link text-6xl text-textColour"
-          to="/experience"
-        >
-          EXPERIENCE
+        <Navbar.Link className="navbar-link text-6xl text-textColour">
+          <Link to="/experience">EXPERIENCE</Link>
         </Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
