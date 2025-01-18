4508776ffdfe8f1bf2d1977857cf5c9cfa61fd23
hanna <hannadhm@hotmail.co.uk>
2025-01-18 14:55:13 +0000

    modify vite.config.ts to make react router intercept 404

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,14 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/hanna-portfolio/",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          "404": ["@vitejs/plugin-vue"],
+        },
+      },
+    },
+  },
   plugins: [react()],
 });
